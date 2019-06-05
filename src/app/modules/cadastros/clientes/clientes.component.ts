7facaeead8c5d9809acfd46c8289243c366afe4c
pamellatrevisan <pamella.trevisan@gmail.com>
2019-06-04 23:57:13 -0300

    - adicionado parametro de inicio de paginação no link de cliente + navegação alterando o inicio da paginação na lista de clientes

--- src/app/modules/cadastros/clientes/clientes.component.ts
+++ src/app/modules/cadastros/clientes/clientes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ClientesService } from './clientes.service';
 
@@ -10,12 +10,32 @@ import { ClientesService } from './clientes.service';
 })
 export class ClientesComponent implements OnInit {
 
+  inscricao: Subscription;
+  TAMANHO_PAGINA: number = 10;
+  inicio: number;
   clientes: any[];
 
-  constructor(private clientesService: ClientesService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private clientesService: ClientesService) { }
 
   ngOnInit() {
+    this.inscricao = this.route.queryParams.subscribe((queryParams: any) => {
+      this.inicio = queryParams['inicio'] == null ? 0 : +queryParams['inicio'];
+    })
     this.clientes = this.clientesService.getClientes();
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  paginar(qtdePaginas: number) {
+    let proximoInicio = this.inicio + (qtdePaginas * this.TAMANHO_PAGINA);
+    if (proximoInicio >= 0) {
+      this.router.navigate(['/cadastros/clientes'], {queryParams: {'inicio': proximoInicio}});
+    }
   }
   
 }
